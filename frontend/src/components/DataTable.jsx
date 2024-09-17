68eed920edb0dbbbc1d7e4ff10b93e961e79ad47
ShravastiSiddharth <91666088+ShravastiSiddharth@users.noreply.github.com>
2024-09-17 14:08:42 +0530

    Chaged API URL to live API url

--- frontend/src/components/DataTable.jsx
+++ frontend/src/components/DataTable.jsx
@@ -8,7 +8,7 @@ const DataTable = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://localhost:5000/api/quadb/get-data');
+        const response = await axios.get('https://quadb-app.onrender.com/api/quadb/get-data');
         setData(response.data);
       } catch (error) {
         console.error('Error fetching data:', error);
